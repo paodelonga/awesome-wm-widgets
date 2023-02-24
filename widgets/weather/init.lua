3c689455f9a07e37da6afce6495b103bbf852939
paodelonga <venombrime@gmail.com>
2023-02-23 22:52:10 -0300

    :bug: update: increase timeout + switch `marker` to `fontfg`

--- widgets/weather/init.lua
+++ widgets/weather/init.lua
@@ -12,24 +12,20 @@
 local async = require("lain.helpers").async
 local newtimer = require("lain.helpers").newtimer
 local wibox = require("wibox")
-local markup = require("lain").util.markup
-
-local function marker(color, font, text)
-    return markup(color, markup.font(font, text))
-end
+local fontfg = require("lain").util.markup.fontfg
 
 local function Widget(arguments)
     local Weather = { widget = wibox.widget.textbox() }
 
     local args = arguments or {}
-    local timeout = args.timeout or 5
+    local timeout = args.timeout or 900
     local font = args.font or { name = "Noto Mono 9", color = "#FAFAFA" }
 
     local command = "curl -s https://wttr.in/?format=%t"
 
     function Weather.Update()
         async(command, function(temp)
-            Weather.widget:set_markup(marker(font.color, font.name, temp))
+            Weather.widget:set_markup(fontfg(font.name, font.color, temp))
         end)
     end
 
